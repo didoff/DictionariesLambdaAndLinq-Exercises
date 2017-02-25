9e845a7980016a296da0c286270f750b6149f93d
Deyan <dlucky95@gmail.com>
2017-02-25 04:08:04 +0200

    MinerTask - MORE WORK NEEDED

---
+++ MinerTask/MinerTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerTask
+{
+    class MinerTask
+    {
+        static void Main(string[] args)
+        {
+            string resourceName = Console.ReadLine();
+            int resourceQuantity = int.Parse(Console.ReadLine());
+
+            // What if inputs are stop?
+
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while(true)
+            {
+                try
+                {
+                    resources.Add(resourceName, resourceQuantity);
+                }
+                catch
+                {
+                    int previous;
+                    bool y = resources.TryGetValue(resourceName, out previous);
+
+                    resources[resourceName] = resourceQuantity + previous;
+
+                }
+                
+                resourceName = Console.ReadLine();
+                if(resourceName != "stop")
+                {
+                    resourceQuantity = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+
+                foreach (var entry in resources)
+            {
+                Console.WriteLine($"{entry.Key} -> {entry.Value}");
+            }
+        }
+    }
+}
